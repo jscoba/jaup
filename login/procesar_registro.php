2600050d78f658bf04ffc01960bc56c697ca7b91
Javier Sáez de la Coba <jscoba@gmail.com>
2013-04-26 17:34:52 +0200

    Procesar registro

--- login/procesar_registro.php
+++ login/procesar_registro.php
@@ -1,11 +1,13 @@
 <?php
 
-$v1=$_POST[user]
-$v2=$_POST[pass]
-$v3=$_POST[name]
-$v4=$_POST[curson].$_POST
-$v5=$_POST[user]
+$v1=$_POST['user'];
+$v2=$_POST['pass'];
+$v3=$_POST['name'];
+$v4=$_POST['curson'].$_POST['letra'].$POST_['nivel'];
+$v5=#Hay que hacer un sistema que le diga 0 o 1 a la base de datos. Además, si es TRUE, se tiene que subir el archivo
+$v6=#Fecha actual en formato MYSQL
+$v7=$POST_['tipo'];
+$v8=#Rutina con javascript para el centro educativo!!!
 
 
-
-$query='INSERT jaup_users '(DNI,Nombre,Apellido1,Apellido2, Nacimiento,Sexo,Hora,Fumador,Idiomas) VALUES ('$v1','$v2','$v3','$v4','$v5','$v6','$v7',$v8,'$v9')
+$query="INSERT jaup_users '(user, pass, name, curso, image, registry_date, type) VALUES ('$v1','$v2','$v3','$v4','$v5','$v6','$v7','$v8')"
